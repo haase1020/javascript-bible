ce7fe7e6c7f76ee78d78dd586947fe7885f5b911
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-14 05:48:23 -0400

    vardeclaration notes continued

--- vardeclaration.js
+++ vardeclaration.js
@@ -13,5 +13,12 @@ d = false;
 console.log(d);
 //can't reassign value
 const e = 'does this work?';
-e = 'nope'; //cant reassign values to const (type error)
-console.log(e);
+// e = 'nope'; //cant reassign values to const (type error)
+// console.log(e);
+
+//practice from lecture #30 Variable declaration and assignment
+//example 1: variable declaration
+var example1;
+// console.log(example2); // will throw a type error
+
+// const example3; //Uncaught SyntaxError: Missing initializer in const declaration
