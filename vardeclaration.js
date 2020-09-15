6912759bd12c711722557df803e6e4ff8c648bfa
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-15 04:39:38 -0400

    variable declaration examples

--- vardeclaration.js
+++ vardeclaration.js
@@ -1,24 +1,62 @@
-let a; // declaration
-const b = 10; //declaration and assignment
-a = 'hello everyone'; //assignment
+//example 1*********************************************
 
-console.log(a, b);
-//can reassign value
-let c = 10;
-c = 5;
-console.log(c);
-//can reassign value
-var d = true;
-d = false;
-console.log(d);
-//can't reassign value
-const e = 'does this work?';
-// e = 'nope'; //cant reassign values to const (type error)
-// console.log(e);
+// let a; // declaration
+// const b = 10; //declaration and assignment
+// a = 'hello everyone'; //assignment
 
-//practice from lecture #30 Variable declaration and assignment
-//example 1: variable declaration
-var example1;
-// console.log(example2); // will throw a type error
+// console.log(a, b);
+// //can reassign value
+// let c = 10;
+// c = 5;
+// console.log(c);
+// //can reassign value
+// var d = true;
+// d = false;
+// console.log(d);
+// //can't reassign value
+// const e = 'does this work?';
+// // e = 'nope'; //cant reassign values to const (type error)
+// // console.log(e);
 
-// const example3; //Uncaught SyntaxError: Missing initializer in const declaration
+// //practice from lecture #30 Variable declaration and assignment
+// //example 1: variable declaration
+// var example1;
+// // console.log(example2); // will throw a type error
+
+// // const example3; //Uncaught SyntaxError: Missing initializer in const declaration
+
+// //example 2*********************************************
+// let myNumber = 10;
+// console.log(myNumber);
+
+// //declaration
+// let myString;
+// console.log(myString);
+// //assignment
+// myString = 'hello from the string'; // expression statement
+// console.log(myString)
+// //reassignment
+// myString = "New String";
+// console.log(myString)
+
+// //example 3*********************************************
+// var myNumber = 10;
+// console.log(myNumber);
+
+// //declaration
+// var myString;
+// console.log(myString);
+// //assignment
+// myString = 'hello from the string'; // expression statement
+// console.log(myString)
+// //reassignment
+// myString = "New String";
+// console.log(myString)
+
+//example 4 - "const"*********************************************
+
+const myBoolean = true;
+console.log(myBoolean);
+
+// myBoolean = false; --> error
+// const myBoolean = false; --> error
