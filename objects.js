bfe5b70e77a0b50ce30f74cf5d26a4b992e5688a
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-17 05:52:11 -0400

    working on objects section

--- objects.js
+++ objects.js
@@ -6,3 +6,45 @@ const myCity = {
   popular: true,
   state: 'Virginia',
 };
+
+console.log(myCity.city); // dot notation
+
+myCity.city = 'Seattle';
+console.log(myCity.city);
+
+myHobbies = {
+  sports: 'walking',
+};
+
+myHobbies.learning = 'reading books';
+
+console.log(myHobbies);
+
+delete myHobbies.learning;
+
+console.log(myHobbies);
+
+const myFamily = {
+  mom: 'ruth',
+  husband: 'jon',
+};
+
+//bracket notation
+console.log(myFamily['mom']);
+const familySon = 'son';
+myFamily[familySon] = 'caleb';
+console.log(myFamily);
+
+//accessing nested properties
+const myLocation = {
+  city: 'alexandria',
+  info: {
+    popular: true,
+    country: 'US',
+  },
+};
+
+console.log(myLocation.info.popular); //true
+
+delete myLocation.info['popular'];
+console.log(myLocation);
