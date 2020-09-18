55cbd2fe387bace115e5f8bca716b45f2e6df26a
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-18 06:05:05 -0400

    functions notes

---
+++ functions.js
@@ -0,0 +1,8 @@
+// function is a reusable set of statements and expressions (it's an object!!)
+// function can be: 1) a variable with a value, 2) assigned as a value to the other variable
+// 3) anonymous, 4) used as argument in the call to the other function,
+// 5) value of the property in the object
+
+// smallest function
+function myFn() {}
+myFn(); //undefined
