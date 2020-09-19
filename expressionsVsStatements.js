2a021eb66b8d13706dab149ef79bb2e10917a5f3
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-19 18:16:19 -0400

    working on expressions vs statements

---
+++ expressionsVsStatements.js
@@ -0,0 +1,13 @@
+// *****************expression --> produces a value
+// 'abc', a + b, 'hello', a <= b || c !== d, myFunction(c, d);
+// assignment expression --> a = 20
+//******************statement
+//let a, const b = 5, if (a > b) {}, for (let i = ; i++; i < 5){
+// if semicolon probably a statement
+
+//******************expression statement
+// "abc"; a = a + 3; d = "hello " + "world";
+//expression turned into statement by semicolon
+
+// statement cannot be turned into the expression
+// expressions can be used as args in function calls
