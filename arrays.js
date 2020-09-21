1c3d8aff82fb11e26cc9c2b810995f4dbb1b2196
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-21 04:41:35 -0400

    complete arrays section

--- arrays.js
+++ arrays.js
@@ -9,3 +9,25 @@ myArray[2] = 'abc';
 console.log(myArray); // [1,2,'abc',4]
 myArray[4] = 'hi';
 console.log(myarray.length); //5
+
+// Array methods****************************
+// Push
+const myArray1 = [1, 2, 3];
+myArray1.push(4); // [1,2,3,4]
+
+// Pop
+myArray1.pop(); //[1,2,3]
+const removed = myArray1.pop(); //3
+
+// Shift
+const myArray2 = [1, 2, 3, 4];
+myArray2.shift(); // [2,3,4]
+delete myArray2[1]; //index 1 will now return 'empty'
+
+// Unshift --> Push method is better
+const myArray3 = [1, 2, 3, 4];
+myArray3.unshift(0); //[0,1,2,3,4]
+
+// '===' compares type AND value (in array case, the pointer)
+console.log(myArray2 === myArray3); // false because different pointers
+console.log(myArray2.toString() === myArray3.toString()); //true because now comparing primitive type "string" //only if order is same
