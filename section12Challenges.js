de5abd1a03104efb39938ccd698dcd197d95c1ae
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-03 07:46:14 -0400

    challenges for closures

--- section12Challenges.js
+++ section12Challenges.js
@@ -1,13 +1,31 @@
-//callbacks, events, events queue and execution contexts stack
-function waitingFn(timeinMs) {
-  const futureTime = Date.now() + timeinMs;
+// //callbacks, events, events queue and execution contexts stack
+// function waitingFn(timeinMs) {
+//   const futureTime = Date.now() + timeinMs;
 
-  while (futureTime > Date.now()) {
-    //waiting...
+//   while (futureTime > Date.now()) {
+//     //waiting...
+//   }
+// }
+// waitingFn(5000);
+
+// setTimeout(() => console.log('callback executed'), 2000);
+
+// console.log('function call has just ended');
+
+//closures
+
+function outerFn(mult) {
+  const a = 10;
+  function innerFn(b) {
+    console.dir(innerFn);
+    return (a + b) * mult;
   }
+
+  return innerFn;
 }
-waitingFn(5000);
 
-setTimeout(() => console.log('callback executed'), 2000);
+const res = outerFn(2);
+console.log(res(20));
 
-console.log('function call has just ended');
+const res2 = outerFn(3);
+console.log(res2(20));
