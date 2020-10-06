60ee77fe2fffac77ce1d07f1964b5ecd289cf216
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-06 04:29:42 -0400

    lesson 134

--- section13Challenges.js
+++ section13Challenges.js
@@ -1,1 +1,21 @@
 // const and let don't work with hoisting
+
+//function declaration --> does declaration, initialization and assignment all together
+function smile() {
+  console.log('😉😉😉');
+}
+
+smile();
+
+//function expression as IIFE
+(function () {
+  console.log('😇😇😇');
+})();
+
+//function expression assigned to a variable --> temporal dead zone beware!!
+// declaration but not initialized. When executed the function is initialized and assigned (see lecture 134)
+const cowboy = function (a) {
+  return a;
+};
+
+console.log(cowboy('🤠🤠🤠')); //returns console log
