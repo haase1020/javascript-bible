05a0f4f59f142060083329c57301e1b7e8563fe6
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-13 04:30:02 -0400

    map method

--- section16arrayHelperMethods.js
+++ section16arrayHelperMethods.js
@@ -1,21 +1,36 @@
 // //✔️ ⏱️ 💻 😉 🌳 📗 💯 ✨
 
 
-// 🌳 For each 
-const myArray = [1,true, "abc"]
+// // 🌳 For each 
+// const myArray = [1,true, "abc"]
 
-myArray.forEach((i) => {
-    console.log(i)
-})
+// myArray.forEach((i) => {
+//     console.log(i)
+// })
 
-const myCities = ["London", "Tokyo", "Osaka"]
+// const myCities = ["London", "Tokyo", "Osaka"]
 
-const arrayInfo = (element, index) => {
-    console.log(element + " is located at the index " + index + " in the array")
-};
+// const arrayInfo = (element, index) => {
+//     console.log(element + " is located at the index " + index + " in the array")
+// };
 
-myCities.forEach((element, index) => {
-    console.log(arrayInfo(element, index))
-})
+// myCities.forEach((element, index) => {
+//     console.log(arrayInfo(element, index))
+// })
 
 // 🌳 map
+const myNumbers = [1,5,7];
+// option 1
+// let mySquareNumbers = myNumbers.map(
+//     element => element * element
+// );
+
+// console.log(mySquareNumbers)
+// console.log(myNumbers)
+
+//option 2
+const squareNumbers = (element, index) => {
+    console.log("element at index " + index + " is " + element);
+    return element * element;
+};
+let mySquareNUmbers = myNumbers.map(squareNumbers);
