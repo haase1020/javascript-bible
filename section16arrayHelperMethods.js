01395eb1fed4b931a5ae128f8a7c073471cf0c7d
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-15 06:09:56 -0400

    challenge 3-6

--- section16arrayHelperMethods.js
+++ section16arrayHelperMethods.js
@@ -174,28 +174,45 @@
 
 
 
-// 💯 challenge 3-5: are arrays equal or not?
+// // 💯 challenge 3-5: are arrays equal or not?
 
-const a = [1, 2, 3];
-const b = [1, 2, 3];
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
 
 
 
-/* Create a function "arraysAreEqual" with two parameters "arrayOne" and "arrayTwo". 
-If two arrays are equal (have same quantity of the elements and all elements match e.g. arrayOne[0] === arrayTwo[0] etc.) return "true".
-Otherwise return "false". */
+// /* Create a function "arraysAreEqual" with two parameters "arrayOne" and "arrayTwo". 
+// If two arrays are equal (have same quantity of the elements and all elements match e.g. arrayOne[0] === arrayTwo[0] etc.) return "true".
+// Otherwise return "false". */
 
-const arraysAreEqual = (arrayOne, arrayTwo) =>
-  arrayOne.length === arrayTwo.length &&
-  arrayOne.every(
-    (element, index) => element === arrayTwo[index]
-  );
+// const arraysAreEqual = (arrayOne, arrayTwo) =>
+//   arrayOne.length === arrayTwo.length &&
+//   arrayOne.every(
+//     (element, index) => element === arrayTwo[index]
+//   );
 
-const c = [2, 1, 3];
-const d = [1, 2, 3, 4];
+// const c = [2, 1, 3];
+// const d = [1, 2, 3, 4];
 
-console.log("_____________________")
+// console.log("_____________________")
 
-console.log(arraysAreEqual(a, b)); // true
-console.log(arraysAreEqual(a, c)); // false
-console.log(arraysAreEqual(a, d)); // false
+// console.log(arraysAreEqual(a, b)); // true
+// console.log(arraysAreEqual(a, c)); // false
+// console.log(arraysAreEqual(a, d)); // false
+
+// 💯 challenge 3-6: check if element is in array?
+
+const transport = ["Bus", "Car", "Bicycle", "Airplane"];
+
+/* Create a function "elementFound" with two parameters "inputArray" and "searchElement".
+If "searchElement" is found in the "inputArray" - return "true".
+Otherwise return "false"
+Hint: Use "some" helper method to solve this
+*/
+
+const elementFound = (inputArray, searchElement) =>
+  inputArray.some(element => element === searchElement);
+
+console.log(elementFound(transport, "Bus")); // true
+console.log(elementFound(transport, "Phone")); // false
+console.log(elementFound(transport, "Airplane")); // true
