73cba2dfc5d46b08965f874278ae917dc9f6c73b
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-17 05:29:49 -0400

    challenge 3-10 solution

--- section16part2.js
+++ section16part2.js
@@ -53,35 +53,88 @@
 // console.log(uniqueFruits(fruits))
 
 
-// 💯 challenge 3-9: reduce array of objects
-const posts = [
+// // 💯 challenge 3-9: reduce array of objects
+// const posts = [
+//     {
+//       title: "How to find JavaScript developer job?",
+//       postId: 3421,
+//       comments: 25
+//     },
+//     {
+//       title: "Is it hard to learn ES6?",
+//       postId: 5216,
+//       comments: 3
+//     },
+//     {
+//       title: "Should I learn React or Angular?",
+//       postId: 8135,
+//       comments: 12
+//     }
+//   ];
+  
+//   const minimalComentsQty = 10;
+  
+//   /* Create a function "popularPostsIds" with two parameters "posts" and "minimalComentsQty".
+//   This function "popularPostsIds" should return an array of postIds of posts that have quantity of "comments" at least equal to "minimalComentsQty"
+//   */
+//   const popularPostsIds = (posts, minimalComentsQty) =>
+//     posts.reduce((postsIds, post) => {
+//       if (post.comments >= minimalComentsQty)
+//         postsIds.push(post.postId);
+//       return postsIds;
+//     }, []);
+  
+//   console.log(popularPostsIds(posts, minimalComentsQty)); // [3421, 8135]
+
+
+
+// 💯 challenge 3-10: reduce to object
+const products = [
     {
-      title: "How to find JavaScript developer job?",
-      postId: 3421,
-      comments: 25
+      title: "Phone case",
+      price: 23,
+      quantity: 2,
+      category: "Accessories"
     },
     {
-      title: "Is it hard to learn ES6?",
-      postId: 5216,
-      comments: 3
+      title: "Android phone",
+      price: 150,
+      quantity: 1,
+      category: "Phones"
     },
     {
-      title: "Should I learn React or Angular?",
-      postId: 8135,
-      comments: 12
+      title: "Headphones",
+      price: 78,
+      quantity: 1,
+      category: "Accessories"
+    },
+    {
+      title: "Sport Watch",
+      price: 55,
+      quantity: 2,
+      category: "Watches"
     }
   ];
   
-  const minimalComentsQty = 10;
-  
-  /* Create a function "popularPostsIds" with two parameters "posts" and "minimalComentsQty".
-  This function "popularPostsIds" should return an array of postIds of posts that have quantity of "comments" at least equal to "minimalComentsQty"
+  /* Create a function "quantityByCategories" with one parameter "products".
+  This function "quantityByCategories" should return an object with keys equal to categories and values equal to sum of all quantities in each category.
   */
-  const popularPostsIds = (posts, minimalComentsQty) =>
-    posts.reduce((postsIds, post) => {
-      if (post.comments >= minimalComentsQty)
-        postsIds.push(post.postId);
-      return postsIds;
-    }, []);
+  const quantityByCategories = products =>
+    products.reduce((counts, product) => {
+      // console.log(
+      //   "Counts is " +
+      //     JSON.stringify(counts) +
+      //     " and product is " +
+      //     JSON.stringify(product)
+      // );
+      counts[product.category] =
+        (counts[product.category] || 0) + product.quantity;
+      return counts;
+    }, {});
   
-  console.log(popularPostsIds(posts, minimalComentsQty)); // [3421, 8135]
+  console.log(quantityByCategories(products));
+  /* {
+    Accessories: 3,
+    Phones: 1,
+    Watches: 2
+  } */
