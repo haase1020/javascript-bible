94e3bc4dd6541e2182e8468c39b3a43d07518e88
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-17 05:19:38 -0400

    complete challenge 3-9

--- section16part2.js
+++ section16part2.js
@@ -38,16 +38,50 @@
 
 // console.log(personNames(persons))
 
-//  📗 EXAMPLE 3: remove duplicates
-const fruits = ["banana", "orange", "apple", "apple",
-"banana", "mango","kiwi"
-]
+// //  📗 EXAMPLE 3: remove duplicates
+// const fruits = ["banana", "orange", "apple", "apple",
+// "banana", "mango","kiwi"
+// ]
 
-const uniqueFruits = arrayOfFruits =>
-arrayOfFruits.reduce((uniqueElements, fruit) => {
-    if (!uniqueElements.includes(fruit))
-    uniqueElements.push(fruit);
-    return uniqueElements
-},[])
+// const uniqueFruits = arrayOfFruits =>
+// arrayOfFruits.reduce((uniqueElements, fruit) => {
+//     if (!uniqueElements.includes(fruit))
+//     uniqueElements.push(fruit);
+//     return uniqueElements
+// },[])
 
-console.log(uniqueFruits(fruits))
+// console.log(uniqueFruits(fruits))
+
+
+// 💯 challenge 3-9: reduce array of objects
+const posts = [
+    {
+      title: "How to find JavaScript developer job?",
+      postId: 3421,
+      comments: 25
+    },
+    {
+      title: "Is it hard to learn ES6?",
+      postId: 5216,
+      comments: 3
+    },
+    {
+      title: "Should I learn React or Angular?",
+      postId: 8135,
+      comments: 12
+    }
+  ];
+  
+  const minimalComentsQty = 10;
+  
+  /* Create a function "popularPostsIds" with two parameters "posts" and "minimalComentsQty".
+  This function "popularPostsIds" should return an array of postIds of posts that have quantity of "comments" at least equal to "minimalComentsQty"
+  */
+  const popularPostsIds = (posts, minimalComentsQty) =>
+    posts.reduce((postsIds, post) => {
+      if (post.comments >= minimalComentsQty)
+        postsIds.push(post.postId);
+      return postsIds;
+    }, []);
+  
+  console.log(popularPostsIds(posts, minimalComentsQty)); // [3421, 8135]
