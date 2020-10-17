c9ab90ad34d7de48f8a8251228034a8d8677c689
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-17 05:11:52 -0400

    example 1,2,3 for section16 part 2

--- section16part2.js
+++ section16part2.js
@@ -1,11 +1,53 @@
-// //✔️ ⏱️ 💻 😉 🌳 📗 💯 ✨
+// // //✔️ ⏱️ 💻 😉 🌳 📗 💯 ✨
 
-//  🌳 reduce()
-const nuNumbers = [5,10,3,15];
+// //  🌳 reduce()
 
-const sum = arrayOfNumbers => arrayOfNumbers.reduce(
-    (accumulator, number) => {
-        console.log("accumulator is " + accumulator + " and number is " + number)
-        return accumulator + number}, 0)
+// //  📗 EXAMPLE 1 : sum numbers
+// const myNumbers = [5,10,3,15];
 
-console.log(sum(myNumbers));
+// const sum = arrayOfNumbers => arrayOfNumbers.reduce(
+//     (accumulator, number) => {
+//         console.log("accumulator is " + accumulator + " and number is " + number)
+//         return accumulator + number}, 0) // 0 stipulates 0 index, otherwise starts at 1 index
+
+// console.log(sum(myNumbers));
+
+// //  📗 EXAMPLE 2: reduce array of object to single array
+
+// const persons = [
+//     {
+//         name: "John",
+//        age: 2 
+//     },
+//     {
+//         name: "Jill",
+//        age: 3 
+//     },
+//     {
+//         name: "Jack",
+//        age: 3 
+//     }
+// ]
+
+// const personNames = arrayOfPersons => 
+// arrayOfPersons.reduce((names, person) => {
+// names.push(person.name);
+// console.log(names, person)
+// return names;
+// },[])
+
+// console.log(personNames(persons))
+
+//  📗 EXAMPLE 3: remove duplicates
+const fruits = ["banana", "orange", "apple", "apple",
+"banana", "mango","kiwi"
+]
+
+const uniqueFruits = arrayOfFruits =>
+arrayOfFruits.reduce((uniqueElements, fruit) => {
+    if (!uniqueElements.includes(fruit))
+    uniqueElements.push(fruit);
+    return uniqueElements
+},[])
+
+console.log(uniqueFruits(fruits))
