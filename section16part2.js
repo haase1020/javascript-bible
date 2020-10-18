62ef7c6a82254c22b983c4ebe55ceba0199a4ea3
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-18 05:25:45 -0400

    sort method

--- section16part2.js
+++ section16part2.js
@@ -88,53 +88,60 @@
 
 
 
-// 💯 challenge 3-10: reduce to object
-const products = [
-    {
-      title: "Phone case",
-      price: 23,
-      quantity: 2,
-      category: "Accessories"
-    },
-    {
-      title: "Android phone",
-      price: 150,
-      quantity: 1,
-      category: "Phones"
-    },
-    {
-      title: "Headphones",
-      price: 78,
-      quantity: 1,
-      category: "Accessories"
-    },
-    {
-      title: "Sport Watch",
-      price: 55,
-      quantity: 2,
-      category: "Watches"
-    }
-  ];
+// // 💯 challenge 3-10: reduce to object
+// const products = [
+//     {
+//       title: "Phone case",
+//       price: 23,
+//       quantity: 2,
+//       category: "Accessories"
+//     },
+//     {
+//       title: "Android phone",
+//       price: 150,
+//       quantity: 1,
+//       category: "Phones"
+//     },
+//     {
+//       title: "Headphones",
+//       price: 78,
+//       quantity: 1,
+//       category: "Accessories"
+//     },
+//     {
+//       title: "Sport Watch",
+//       price: 55,
+//       quantity: 2,
+//       category: "Watches"
+//     }
+//   ];
   
-  /* Create a function "quantityByCategories" with one parameter "products".
-  This function "quantityByCategories" should return an object with keys equal to categories and values equal to sum of all quantities in each category.
-  */
-  const quantityByCategories = products =>
-    products.reduce((counts, product) => {
-      // console.log(
-      //   "Counts is " +
-      //     JSON.stringify(counts) +
-      //     " and product is " +
-      //     JSON.stringify(product)
-      // );
-      counts[product.category] =
-        (counts[product.category] || 0) + product.quantity;
-      return counts;
-    }, {});
+//   /* Create a function "quantityByCategories" with one parameter "products".
+//   This function "quantityByCategories" should return an object with keys equal to categories and values equal to sum of all quantities in each category.
+//   */
+//   const quantityByCategories = products =>
+//     products.reduce((counts, product) => {
+//       // console.log(
+//       //   "Counts is " +
+//       //     JSON.stringify(counts) +
+//       //     " and product is " +
+//       //     JSON.stringify(product)
+//       // );
+//       counts[product.category] =
+//         (counts[product.category] || 0) + product.quantity;
+//       return counts;
+//     }, {});
   
-  console.log(quantityByCategories(products));
-  /* {
-    Accessories: 3,
-    Phones: 1,
-    Watches: 2
-  } */
+//   console.log(quantityByCategories(products));
+//   /* {
+//     Accessories: 3,
+//     Phones: 1,
+//     Watches: 2
+//   } */
+
+
+//  🌳 sort()
+
+const myNumbers = [10,5,7,12,20];
+
+console.log(myNumbers.sort((a,b) => a-b))
