61cab979bfe2f16cb22a1ac036a173960c79e166
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-20 05:50:32 -0400

    example 1 for spread operator

--- section17rest.js
+++ section17rest.js
@@ -13,14 +13,30 @@
 // console.log(sumNumbers("abc",1,2,3,4,5,6,7))
 
 
-// ✔️ reular function
-function sumNumbers(...nums){
-    //option 1 = - rest parameter
-    console.log(nums); //array
-    console.log(arguments) //this is now an object, so much convert to array
-    return nums
-    .filter(num => typeof num === "number")
-    .reduce((sum,num) => sum + num, 0)
-};
+// // ✔️ regular function
+// function sumNumbers(...nums){
+//     //option 1 = - rest parameter
+//     console.log(nums); //array
+//     console.log(arguments) //this is now an object, so much convert to array
+//     return nums
+//     .filter(num => typeof num === "number")
+//     .reduce((sum,num) => sum + num, 0)
+// };
 
-console.log(sumNumbers("abc",1,2,3,4,5,6,7))
+// console.log(sumNumbers("abc",1,2,3,4,5,6,7))
+
+
+//  🌳 spread operator
+
+// // 📗 example 1
+// const myGreeting = (name, city, age) => {
+//     return `hello from ${name} who is ${age} years old and lives in ${city}`;
+// };
+
+// const myPerson = ["Alice", "Boston", 100];
+// console.log(myGreeting(...myPerson));
+
+// const anotherPerson = ["NewYork", 200]
+// console.log(myGreeting("Bob", ...anotherPerson))
+
+// 📗 example 2
