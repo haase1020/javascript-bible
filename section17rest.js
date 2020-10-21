44c11ca1162f4c116addbb7be86ecbb3d66d7383
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-21 05:54:11 -0400

    complete challenge 5-1

--- section17rest.js
+++ section17rest.js
@@ -55,28 +55,69 @@
 // const date = new Date(...dateInfo);
 // console.log(date)
 
-// 📗 example 4 - copy array
-const myArray = ["a", 5, [], true];
+// // 📗 example 4 - copy array
+// const myArray = ["a", 5, [], true];
 
-const newArray = [...myArray];
-newArray.push(10); //myArray is not mutates
-newArray[2].push(7) //myArray is mutated
-console.log(newArray);
+// const newArray = [...myArray];
+// newArray.push(10); //myArray is not mutates
+// newArray[2].push(7) //myArray is mutated
+// console.log(newArray);
 
-// 📗 example 5 - copy object (ES2018)
-const myObject = {a:10,b:"c", d:[1,2]};
-const copiedObject = {...myObject };
-copiedObject.a=20; //myObject is not mutated
-copiedObject.d.pop(); //myObject is mutated
-console.log(copiedObject);
-console.log(myObject)
+// // 📗 example 5 - copy object (ES2018)
+// const myObject = {a:10,b:"c", d:[1,2]};
+// const copiedObject = {...myObject };
+// copiedObject.a=20; //myObject is not mutated
+// copiedObject.d.pop(); //myObject is mutated
+// console.log(copiedObject);
+// console.log(myObject)
 
 
-// 📗 example 6 - spread string
+// // 📗 example 6 - spread string
 
-const myStr = "hello world";
+// const myStr = "hello world";
 
-const letters = [...myStr];
-console.log("letters",letters);
-const oldStyleLetters = myStr.split("");
-console.log(oldStyleLetters)
+// const letters = [...myStr];
+// console.log("letters",letters);
+// const oldStyleLetters = myStr.split("");
+// console.log(oldStyleLetters)
+
+
+//💯 challenge 5-1
+/*
+Create a function "meanScore" that will accept any quantity of the arguments, gather them into single array and return mean value of all arguments rounded to 2 decimal places.
+If at least one element in the gathered array is not a number - throw following error to the console: 
+"Supplied arguments must contain only numbers!"
+Type of the returned value must be a "number".
+HINT: In this Challenge you should use both rest and spread operators.
+*/
+const meanScore = (...scores) => {
+    if (scores.every(score => typeof score ==="number")) {
+        ///calculate mean
+        return parseFloat(scores.reduce((avg, score) => {
+            return avg + score/ scores.length;
+        }, 0).toFixed(2)
+        );
+    } else {
+        throw new Error (
+            "args must contain numbers!"
+        )
+    }
+}
+const scores1 = [0, 1.5, 2.5, 3.7];
+const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2];
+const scores3 = [1.3, 2.5, 1.9];
+const scores4 = ["abc", 1.3, true, 2.5, 1.9];
+
+
+
+console.log(meanScore(...scores1)); // 1.93
+
+console.log(
+  meanScore(...scores1, ...scores2)
+); // 2.8
+
+console.log(
+  meanScore(...scores1, ...scores2, ...scores3)
+); // 2.59
+
+console.log(meanScore(...scores4)); // Supplied arguments must contain only numbers!
