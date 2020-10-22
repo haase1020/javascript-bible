bd7e95b05b4d0efb9f058392f58f1a6bc0495538
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-22 05:32:08 -0400

    challenge 5-2

--- section17rest.js
+++ section17rest.js
@@ -169,3 +169,34 @@
 // btn("button 1", "pink")
 // btn("button 2")
 // btn()
+
+
+//💯 challenge 5-2
+// /*
+/* Create a function "weatherForecast" with two parameters "city" and "weather". 
+If second argument is absent, "weather" parameter should get value "Great weather!".
+1. First solve this WITHOUT default function parameter.
+2. Comment previous solution and solve same task WITH default function parameter.
+NOTE: Carefully compare your results with test calls results
+*/
+//  const weatherForecast = (city, weather) => {
+//     //  weather = weather ||"great weather" //could return different results
+//     weather = 
+//     weather !== undefined ? weather: "great weather!"
+//     return `waether forecast for ${city}: ${weather}`;
+//  };
+const weatherForecast = (city, weather="great weather!") => `Weather forecast for ${city}: ${weather}`
+console.log(weatherForecast("Dubai", "Sunny"));
+// Weather forecast for Dubai: Sunny
+
+console.log(weatherForecast("London", "Light rain"));
+// Weather forecast for London: Light rain
+
+console.log(weatherForecast("Paris"));
+// Weather forecast for Paris: Great weather!
+
+console.log(weatherForecast("Miami", ""));
+// Weather forecast for Miami:
+
+console.log(weatherForecast("Las Vegas", undefined));
+// Weather forecast for Las Vegas: Great weather!
