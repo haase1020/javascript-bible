c6846ff76af4bbc247bfd855d10e9dbb2ebff991
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-25 13:58:00 -0400

    native prototypes lecture

---
+++ section20part1.js
@@ -0,0 +1,26 @@
+// ✔️ ⏱️ 💻 😉  📗🌳 💯 ✨
+
+// 🌳 programming language types
+/*
+class based: C++, Java
+protype-based: JavaScript, ES6 introduced classes (syntactic sugar)
+*/
+
+/*🌳 each object in JS has Prototype
+[[Prototype]] --> internal hidden property of each object
+__proto__ --> visible property of each object | proto can be either object or null | getter/setter for hidden property [[Prototype]]
+*/
+
+/*🌳 what is Prototype?
+Object.prototype --> top-level prototype ✨Object is a function✨
+✨ Object.prototype.__proto__ = null ✨
+Object.prototype --> object | ✨includes constructor function✨
+*/
+
+/*🌳 native prototypes
+object, array, function, data, string,
+number, boolean, regexp, promise, map, set, error
+✨ arrow functions do not have prototype method
+✨ every function is an instance for Function.prototype 
+✨ every fn in JS (except arrow fns) is a constructor fn
+*/
