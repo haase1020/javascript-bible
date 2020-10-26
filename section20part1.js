8babb3bb8cd17d860af649f6438d5051dd313410
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-26 05:15:37 -0400

    challenge 7-1

--- section20part1.js
+++ section20part1.js
@@ -72,24 +72,140 @@ const newInstance = new MyFunctionConstructor(props);
 */
 
 
-// 🌳 Object.create()
-const Parent = {
-    type: "Parent",
-    typeInfo(){
-        console.log(`Hello from ${this.type}`);
-    },
-    modifyType(newType) {
-        this.type = newType;
-    }
-};
-console.log(Parent.type)
-Parent.typeInfo();
-console.log(Parent.prototype);// undefined
+// // 🌳 Object.create()
+// const Parent = {
+//     type: "Parent",
+//     typeInfo(){
+//         console.log(`Hello from ${this.type}`);
+//     },
+//     modifyType(newType) {
+//         this.type = newType;
+//     }
+// };
+// console.log(Parent.type)
+// Parent.typeInfo();
+// console.log(Parent.prototype);// undefined
 
-const child = Object.create(Parent);
-console.log(child.__proto__===Parent); //true
+// const child = Object.create(Parent);
+// console.log(child.__proto__===Parent); //true
 
-child.type = "Child";
-child.typeInfo();
-child.modifyType("modified child");
-child.typeInfo();
+// child.type = "Child";
+// child.typeInfo();
+// child.modifyType("modified child");
+// child.typeInfo();
+
+
+/*
+🌳 imperative programming paradigm:🌳
+insert new protoype to the prototype chain:
+1. created new fn for new prototype
+2. reassign value of the "prototype using "Object.create"
+3. make "constructor" property equal to the fn itself
+4. call "parent" fn if it should add new props to each new instance
+
+*/
+
+/**
+ * 💯 CHALLENGE 7-1
+ *
+ * Insert new prototype "Vehicle.prototype" to the existing prototype chain
+ *
+ * CURRENT CHAIN
+ * CivilPlane.prototype > Airplane.prototype
+ * > Object.protoype
+ *
+ * UPDATED CHAIN
+ * CivilPlane.prototype > Airplane.prototype
+ * > Vehicle.prototype > Object.protoype
+ *
+ * Vehicle.prototype should set two additional
+ * properties:
+ * 1. maxSpeed
+ * 2. weight
+ *
+ * It should also contain method "vehicleInfo"
+ * available for any object down the prototype chain
+ */
+
+function Vehicle(props) {
+    this.maxSpeed = props.maxSpeed;
+    this.weight = props.weight;
+  }
+  
+  Vehicle.prototype.vehicleInfo = function() {
+    console.log(
+      `Max speed of the vehicle is ${
+        this.maxSpeed
+      } and weight is ${this.weight}`
+    );
+  };
+  
+  function Airplane(props) {
+    Vehicle.call(this, props);
+    this.wingspan = props.wingspan;
+    this.maxRangeOfFlight = props.maxRangeOfFlight;
+  }
+  
+  Airplane.prototype = Object.create(Vehicle.prototype);
+  
+  Airplane.prototype.constructor = Airplane;
+  
+  Airplane.prototype.airplaneInfo = function() {
+    console.log(
+      `Wingspan of the airplane is ${
+        this.wingspan
+      } and maximal range of flight is ${
+        this.maxRangeOfFlight
+      }`
+    );
+  };
+  
+  function CivilPlane(props) {
+    Airplane.call(this, props);
+    this.numberOfSeats = props.numberOfSeats;
+  }
+  
+  CivilPlane.prototype = Object.create(Airplane.prototype);
+  
+  CivilPlane.prototype.constructor = CivilPlane;
+  
+  CivilPlane.prototype.seatsInfo = function() {
+    console.log(this); // instance of the prototype
+    console.log(
+      `Number of seats in the plane is ${this.numberOfSeats}`
+    );
+  };
+  
+  CivilPlane.prototype.modifySeatsNumber = function(
+    newSeatsQty
+  ) {
+    this.numberOfSeats = newSeatsQty;
+  };
+  
+  const propsForSmallPlane = {
+    numberOfSeats: 4,
+    wingspan: 20,
+    maxRangeOfFlight: 1000,
+    maxSpeed: 800,
+    weight: 15
+  };
+  
+  const smallPlane = new CivilPlane(propsForSmallPlane);
+  
+  const propsForLargePlane = {
+    numberOfSeats: 250,
+    wingspan: 60,
+    maxRangeOfFlight: 3500,
+    maxSpeed: 900,
+    weight: 40
+  };
+  
+  const largePlane = new CivilPlane(propsForLargePlane);
+  
+  /**
+   * VERIFICATION
+   */
+  smallPlane.vehicleInfo(); // Max speed of the vehicle is 800 and weight is 15
+  
+  largePlane.vehicleInfo(); // Max speed of the vehicle is 900 and weight is 40
+  
