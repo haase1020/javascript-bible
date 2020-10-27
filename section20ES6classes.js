d79640bec495a7507dd4209ef9c38a93d40da3a3
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-27 05:54:43 -0400

    complete lecture for classes vs. function constructors

--- section20ES6classes.js
+++ section20ES6classes.js
@@ -4,3 +4,52 @@
 🌳Classes🌳 don't change prototypal inheritance behavior.
 🌳It's just more simplete and declarative way to create Prototype Chain
 */
+
+/*
+class MyClass {✨class is not object literal!!✨
+    constructor(props) { ✨ reserved word "constructor"✨
+        this.title = props.title;
+        this.qty = props.pty;
+    }
+✨ no comma here✨
+info() { ✨method of the prototype(not own property of each instance)✨
+    console.log(`Quantity is ${this.qty})
+}
+}
+*/
+
+/*
+ 🌳function constructors 
+function MyConstructor(props) {
+    this.title = props.title;;
+    this.qty = props.qty;
+}
+
+MyConstructor.prototype.info = function() { ✨adding method with function constructor ✨
+    console.log(`quantity is ${this.qty}`);
+};
+const props = {
+    title: "test title",
+    qty: 5
+}
+const newInstance = new MyConstructor(props);
+newInstance.info; //quatntity is 5
+
+vs 🌳Classes 
+class MyClass {
+    constructor(props) {
+       this.title = props.title;;
+       this.qty = props.qty;
+    }
+    info() {
+        console.log(`quantity is ${this.qty}`); ✨adding method with classes ✨
+    }
+}
+const props = {
+    title: "test title",
+    qty: 5
+}
+const newInstance = new MyClass(props); // that's all- you don't need to write newinstance.info
+✨ refer to classes vs. function constructors lecture to see how to extend another class✨
+
+*/
